946bcac92afc79848ab17d0fd3283b3dd610c06a
Harm te Hennepe <dhtehennepe@gmail.com>
2018-12-06 21:26:27 +0100

    Add day6 part2

--- 2018/day6.pl
+++ 2018/day6.pl
@@ -1,22 +1,27 @@
 #!/usr/bin/perl
 use 5.026;
-use List::Util qw(max);
+use List::Util qw(max sum);
 use List::UtilsBy qw(min_by);
 use List::MoreUtils qw(minmax);
 
-my @coords;
+my (@coords, $nearAll);
 push @coords, scalar {x => /\G(\d+)/, y => /, (\d+)/} foreach <>;
 
 my ($minX, $maxX) = minmax map {$_->{x}} @coords;
 my ($minY, $maxY) = minmax map {$_->{y}} @coords;
 
-foreach my $x ($minX - 1 .. $maxX + 1) {
-    foreach my $y ($minY - 1 .. $maxY + 1) {
-        my @bestCoords = min_by {abs($_->{x} - $x) + abs($_->{y} - $y)} @coords;
-        if (@bestCoords == 1) {
-            if ($x >= $minX and $x <= $maxX and $y >= $minY and $y <= $maxY) {
-                $bestCoords[0]->{score}++;
-            } else {
-                $bestCoords[0]->{score} = '-inf'; } } } }
+foreach my $x ($minX..$maxX) {
+    foreach my $y ($minY..$maxY) {
+        my $score = sub {abs($_[0]->{x} - $x) + abs($_[0]->{y} - $y);};
+        my $normal = sub {$x > $minX and $x < $maxX and $y > $minY and $y < $maxY;};
+
+        my @bestCoords = min_by {$score->($_)} @coords;
+        $bestCoords[0]->{score} = $normal->()
+                                ? $bestCoords[0]->{score} + 1
+                                : '-inf'
+                                if @bestCoords == 1;
+
+        $nearAll++ if sum(map {$score->($_)} @coords) < 10000 } };
 
 say max map {$_->{score}} @coords;
+say $nearAll;
