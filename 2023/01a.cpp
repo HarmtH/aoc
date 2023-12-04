248dccb79cb7dc001287d1431241e0830c6eaccb
Harm te Hennepe <dhtehennepe@gmail.com>
2023-12-04 01:55:21 +0100

    Improve 01/2023

--- 2023/01a.cpp
+++ 2023/01a.cpp
@@ -1,15 +1,14 @@
-#include <cctype>
 #include <iostream>
-#include <sstream>
 #include <string>
+#include <utility>
 
 int main (int argc, char *argv[]) {
     int sum{0};
     std::string line;
     while (std::getline(std::cin, line)) {
-        auto first_pos = line.find_first_of("0123456789");
-        auto last_pos = line.find_last_of("0123456789");
-        sum += line[first_pos] * 10 + line[last_pos];
+        auto first = line[line.find_first_of("0123456789")] - '0';
+        auto last = line[line.find_last_of("0123456789")] - '0';
+        sum += 10 * first + last;
     }
     std::cout << sum << std::endl;
     return 0;
--- 2023/01b.cpp
+++ 2023/01b.cpp
@@ -1,51 +1,44 @@
-#include <cctype>
 #include <iostream>
-#include <sstream>
 #include <string>
+#include <utility>
+#include <vector>
 
 int main (int argc, char *argv[]) {
     int sum{0};
     std::string line;
     while (std::getline(std::cin, line)) {
         size_t maxpos{0}, pos, minpos{std::string::npos};
-        int first{-1}, last{-1};
-        if ((pos = line.find("one")) < minpos) { minpos = pos; first = 1; }
-        if ((pos = line.find("two")) < minpos) { minpos = pos; first = 2; }
-        if ((pos = line.find("three")) < minpos) { minpos = pos; first = 3; }
-        if ((pos = line.find("four")) < minpos) { minpos = pos; first = 4; }
-        if ((pos = line.find("five")) < minpos) { minpos = pos; first = 5; }
-        if ((pos = line.find("six")) < minpos) { minpos = pos; first = 6; }
-        if ((pos = line.find("seven")) < minpos) { minpos = pos; first = 7; }
-        if ((pos = line.find("eight")) < minpos) { minpos = pos; first = 8; }
-        if ((pos = line.find("nine")) < minpos) { minpos = pos; first = 9; }
-        if ((pos = line.rfind("one")) >= maxpos && pos != std::string::npos) { maxpos = pos; last = 1; }
-        if ((pos = line.rfind("two")) >= maxpos && pos != std::string::npos) { maxpos = pos; last = 2; }
-        if ((pos = line.rfind("three")) >= maxpos && pos != std::string::npos) { maxpos = pos; last = 3; }
-        if ((pos = line.rfind("four")) >= maxpos && pos != std::string::npos) { maxpos = pos; last = 4; }
-        if ((pos = line.rfind("five")) >= maxpos && pos != std::string::npos) { maxpos = pos; last = 5; }
-        if ((pos = line.rfind("six")) >= maxpos && pos != std::string::npos) { maxpos = pos; last = 6; }
-        if ((pos = line.rfind("seven")) >= maxpos && pos != std::string::npos) { maxpos = pos; last = 7; }
-        if ((pos = line.rfind("eight")) >= maxpos && pos != std::string::npos) { maxpos = pos; last = 8; }
-        if ((pos = line.rfind("nine")) >= maxpos && pos != std::string::npos) { maxpos = pos; last = 9; }
-        std::stringstream ss(line);
-        size_t i = 0;
-        while (!ss.eof()) {
-            char c; ss >> c;
-            if (std::isdigit(c)) {
-                int val = c - '0';
-                if (i < minpos) {
-                    minpos = i;
-                    first = val;
-                }
-                if (i >= maxpos) {
-                    maxpos = i;
-                    last = val;
-                }
+        int first, last;
+
+        const std::vector<std::pair<std::string, int>> numbers = {
+            {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5},
+            {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}
+        };
+
+        for (const auto& [s, i] : numbers) {
+            pos = line.find(s);
+            if (pos < minpos) {
+                minpos = pos;
+                first = i;
             }
-            i++;
+
+            pos = line.rfind(s);
+            if (pos != std::string::npos && pos >= maxpos) {
+                maxpos = pos;
+                last = i;
+            }
         }
-        sum += first * 10 + last;
+
+        pos = line.find_first_of("123456789");
+        if (pos < minpos) first = line[pos] - '0';
+
+        pos = line.find_last_of("123456789");
+        if (pos != std::string::npos && pos >= maxpos) last = line[pos] - '0';
+
+        sum += 10 * first + last;
     }
+
     std::cout << sum << std::endl;
+
     return 0;
 }
