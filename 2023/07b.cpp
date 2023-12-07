9a1e7d345a3c80ac96e632af3f423fb94ca23f39
Harm te Hennepe <dhtehennepe@gmail.com>
2023-12-07 09:57:56 +0100

    07/2023

---
+++ 2023/07b.cpp
@@ -0,0 +1,86 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <vector>
+
+struct hand_t {
+    enum { HIGH_CARD, ONE_PAIR, TWO_PAIR, THREE_OF_A_KIND,
+        FULL_HOUSE, FOUR_OF_A_KIND, FIVE_OF_A_KIND };
+    std::vector<int> cards;
+    int bid, score;
+
+    void calc_score() {
+        std::map<int, int> card2freq;
+        for (const auto& card : cards) card2freq[card]++;
+        int jokers = card2freq[1];
+        card2freq[1] = 0;
+
+        std::vector<int> freqs;
+        for (const auto& [k, v] : card2freq) freqs.push_back(v);
+        std::sort(freqs.begin(), freqs.end(), std::greater<int>());
+
+        freqs.at(0) += jokers;
+
+        if (freqs.at(0) == 5) score = FIVE_OF_A_KIND;
+        else if (freqs.at(0) == 4) score = FOUR_OF_A_KIND;
+        else if (freqs.at(0) == 3 && freqs.at(1) == 2) score = FULL_HOUSE;
+        else if (freqs.at(0) == 3) score = THREE_OF_A_KIND;
+        else if (freqs.at(0) == 2 && freqs.at(1) == 2) score = TWO_PAIR;
+        else if (freqs.at(0) == 2) score = ONE_PAIR;
+        else score = HIGH_CARD;
+    }
+
+    friend std::istream& operator>>(std::istream& in, hand_t& hand) {
+        for (int i{0}; i < 5; i++) {
+            char c; in >> c;
+            int val;
+            if (std::isdigit(c)) val = c - '0';
+            else if (c == 'T') val = 10;
+            else if (c == 'J') val = 1;
+            else if (c == 'Q') val = 12;
+            else if (c == 'K') val = 13;
+            else if (c == 'A') val = 14;
+            hand.cards.push_back(val);
+        }
+        hand.calc_score();
+
+        in >> hand.bid;
+
+        return in;
+    }
+
+    bool operator<(const hand_t& rhs) const {
+        if (score != rhs.score) {
+            return score < rhs.score;
+        } else {
+            for (int i{0}; i < cards.size(); i++) {
+                if (cards[i] != rhs.cards[i])
+                    return cards[i] < rhs.cards[i];
+            }
+            return false;
+        }
+    }
+};
+
+int main (int argc, char *argv[]) {
+    std::string line;
+    std::vector<hand_t> hands;
+
+    while (std::getline(std::cin, line)) {
+        std::stringstream ss(line);
+        hand_t hand; ss >> hand;
+        hands.push_back(hand);
+    }
+    std::sort(hands.begin(), hands.end());
+
+    long sum{0};
+    for (int i{0}; i < hands.size(); i++) {
+        sum += hands[i].bid * (i + 1);
+    }
+
+    std::cout << sum << std::endl;
+
+    return 0;
+}
