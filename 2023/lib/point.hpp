6bb177b96b3db956ca3f712b38303475e5fb1203
Harm te Hennepe <dhtehennepe@gmail.com>
2023-12-11 13:06:21 +0100

    Update libraries

--- 2023/lib/point.hpp
+++ 2023/lib/point.hpp
@@ -1,32 +1,36 @@
-#include <map>
-#include <iostream>
-#include <vector>
 #include <climits>
 #include <cstdlib>
+#include <iostream>
+#include <map>
+#include <vector>
 
 struct point_t {
-    int y = 0;
-    int x = 0;
+    long y;
+    long x;
+    enum nb_dir_t { STRAIGHT, ALL };
+    static const std::map<std::string, const point_t> dirs;
+    static const std::map<std::string, const point_t> straight_dirs;
 
-    static const std::map<const char*, const point_t> dirs;
-
-    point_t(const int _y, const int _x) : y{_y}, x{_x} {};
+    point_t() : y{0}, x{0} {};
+    point_t(const long _y, const long _x) : y{_y}, x{_x} {};
     point_t(const point_t& rhs) : y{rhs.y}, x{rhs.x} {};
+    point_t(const std::string& dir) { *this = dirs.at(dir); };
     point_t(const char* dir) { *this = dirs.at(dir); };
+    point_t(const char dir) { *this = dirs.at(std::string{dir}); };
 
-    int dist(void) const { return abs(y) + abs(x); }
-    std::vector<point_t> get_valid_neighbours(int ys, int xs) const {
+    bool is_valid(long ys, long xs) const { return 0 <= y && y < ys && 0 <= x && x < xs; }
+    long dist(void) const { return labs(y) + labs(x); }
+    long dist(const point_t& rhs) const { return labs(y - rhs.y) + labs(x - rhs.x); }
+    std::vector<point_t> get_valid_neighbours(long ys, long xs, nb_dir_t nb_dir = ALL) const {
         std::vector<point_t> neighbours;
-        for (const auto& [_, dir] : dirs) {
+        for (const auto& [_, dir] : (nb_dir == STRAIGHT ? straight_dirs : dirs)) {
             auto new_point = *this + dir;
-            if (0 <= new_point.y && new_point.y < ys &&
-                0 <= new_point.x && new_point.x < xs)
-                neighbours.push_back(new_point);
+            if (new_point.is_valid(ys, xs)) neighbours.emplace_back(std::move(new_point));
         }
         return neighbours;
     }
-    std::vector<point_t> get_neighbours() const {
-        return get_valid_neighbours(INT_MAX, INT_MAX);
+    std::vector<point_t> get_neighbours(nb_dir_t nb_dir = ALL) const {
+        return get_valid_neighbours(INT_MAX, INT_MAX, nb_dir);
     }
 
     point_t operator=(const point_t& rhs) { x=rhs.x; y=rhs.y; return *this; }
@@ -41,8 +45,8 @@ struct point_t {
         throw;
     }
     point_t operator*=(const char d) { *this = *this * d; return *this; }
-    point_t operator*(const int n) const { return {n * x, n * y}; } // scale point
-    point_t operator*=(const int n) { *this = *this * n; return *this; }
+    point_t operator*(const long n) const { return {n * x, n * y}; } // scale point
+    point_t operator*=(const long n) { *this = *this * n; return *this; }
 
     bool operator==(const point_t& rhs) const { return x == rhs.x && y == rhs.y; }
     bool operator<(const point_t& rhs) const {
@@ -54,6 +58,9 @@ struct point_t {
     }
 };
 
-inline const std::map<const char*, const point_t>point_t::dirs = {
+inline const std::map<std::string , const point_t>point_t::dirs = {
         {"E", {0,1}}, {"W", {0,-1}}, {"N", {-1,0}}, {"S", {1,0}},
         {"NE", {-1,1}}, {"SE", {1,1}}, {"NW", {-1,-1}}, {"SW", {1,-1}} };
+
+inline const std::map<std::string, const point_t>point_t::straight_dirs =
+    {{"N", {-1,0}}, {"E", {0,1}}, {"S", {1,0}}, {"W", {0,-1}}};
---
+++ 2023/lib/prelude.hpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+#define eb emplace_back
+#define pb push_back
+
+using namespace std;
+namespace r = std::ranges;
+namespace rv = std::ranges::views;
