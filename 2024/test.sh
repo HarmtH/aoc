a5207c13ac1323ab9de326147fa60dd80d6fd17c
Harm te Hennepe <dhtehennepe@gmail.com>
2024-12-03 03:09:12 +0100

    Prepare 2024

---
+++ 2024/test.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ -z "$T" ]; then T=$(echo $1.t*); fi
+for TESTFILE in $T; do
+    if ! [ -e $TESTFILE ]; then
+        echo no tests
+        continue
+    fi
+    echo "./$1.rb < $TESTFILE"
+    ANS=$(./$1.rb < $TESTFILE)
+    echo "$ANS"
+    REALANS=$(echo "$ANS" | tail -n 1)
+    CMPFILE=$(echo $TESTFILE | sed 's/\.t\([0-9]\)\+$/\.a\1/')
+    if [ $TESTFILE != $CMPFILE ] && [ -e $CMPFILE ]; then
+        CMPANS=$(cat $CMPFILE)
+        if [ "$REALANS" = "$CMPANS" ]; then
+            echo pass
+        else
+            echo FAIL: $CMPANS
+        fi
+    else
+        echo no cmp file
+    fi
+    echo ---
+done
