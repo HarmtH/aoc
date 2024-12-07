a3c9c7a96712894c3afff62cd6566872d66f295b
Harm te Hennepe <dhtehennepe@gmail.com>
2024-12-07 17:30:41 +0100

    Make 07 fully functional

--- 2024/07b.rb
+++ 2024/07b.rb
@@ -6,16 +6,17 @@ class Integer
   end
 end
 
-sum = 0
-ARGF.each_line do |line|
-  test_value, remaining_numbers = line.split(":")
-  test_value = test_value.to_i
-  remaining_numbers = remaining_numbers.split.map(&:to_i)
-  sum += test_value if
-    [:+, :*, :|].repeated_permutation(remaining_numbers.size - 1).any?{|ops|
-      test_value == remaining_numbers[1..].zip(ops)
-        .reduce(remaining_numbers[0]){|res, (val, op)| res.send(op, val)}
-    }
-end
-
-puts sum
+puts ARGF
+  .each_line
+  .map{|line|
+    test_value, remaining_numbers = line.split(":")
+    [test_value.to_i, remaining_numbers.split.map(&:to_i)]}
+  .filter_map{|test_value, remaining_numbers|
+    test_value if [:+, :*, :|]
+      .repeated_permutation(remaining_numbers.size - 1)
+      .any?{|ops|
+        test_value == ops
+          .zip(remaining_numbers[1..])
+          .reduce(remaining_numbers[0]) {|res, op|
+            res.send(*op)}}}
+  .sum
