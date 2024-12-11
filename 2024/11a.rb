19930f17a0e78df7c949de3ff599f7f10de2ec46
Harm te Hennepe <dhtehennepe@gmail.com>
2024-12-11 10:27:35 +0100

    Add 11/2024

---
+++ 2024/11a.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+
+# n2a = (stone) number to amount (of that stone number)
+n2a = Hash.new(0)
+
+ARGF.each_line.first.split.each { |num| n2a[num.to_i] += 1 }
+
+75.times do
+  new_n2a = Hash.new(0)
+  n2a.each do |n, a|
+    if n == 0
+      new_n2a[1] += a
+    elsif (len = n.to_s.length).even?
+      new_n2a[n.to_s[0...len / 2].to_i] += a
+      new_n2a[n.to_s[len / 2..].to_i] += a
+    else
+      new_n2a[n * 2024] += a
+    end
+  end
+  n2a = new_n2a
+end
+
+puts n2a.values.sum
