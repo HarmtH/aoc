092a6589986dc4bbff733da6fb46f2bd8a5b5258
Harm te Hennepe <dhtehennepe@gmail.com>
2024-12-13 14:42:13 +0100

    Add 13/2024

---
+++ 2024/13a.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby
+
+require 'matrix'
+
+M = Matrix.zero(2)
+puts ARGF.each_line.with_index.map { |line, idx|
+  idx %= 4
+  tokens = 0
+  case idx
+  when 0
+    M[0, 0], M[1, 0] = line.scan(/\d+/).map(&:to_i)
+  when 1
+    M[0, 1], M[1, 1] = line.scan(/\d+/).map(&:to_i)
+  when 2
+    b = Vector[*line.scan(/\d+/).map(&:to_i)]
+    sol = M.lup.solve(b)
+    tokens = (3 * sol[0] + sol[1]).to_i if sol.all? { |x| x == x.to_i }
+  end
+  tokens
+}.sum
