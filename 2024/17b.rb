695538e6f968eebf7942336380c20131e1f9a47b
Harm te Hennepe <dhtehennepe@gmail.com>
2024-12-19 03:29:09 +0100

    Improve 17b?

--- 2024/17b.rb
+++ 2024/17b.rb
@@ -1,24 +1,23 @@
 #!/usr/bin/ruby
 
 def check(a, subprogram)
-  subprogram.size.times do |i|
+  subprogram.size.times.all? do |i|
     b = (a & 0b111) ^ 0b010
     c = (a >> b) & 0b111
-    b = b ^ c ^ 0b011
-    return false if subprogram[i] != b
+    d = b ^ c ^ 0b011
     a = a >> 3
+    subprogram[i] == d
   end
-  true
 end
 
 def calc(value, pos, program)
-  8.times do |i|
-    next if !check(value + i, program[pos..])
-    return value + i if pos == 0
-    r = calc((value + i) << 3, pos - 1, program)
-    return r if r
-  end
-  false
+  (value ... value + 8)
+    .filter{|v| check(v, program[pos..])}
+    .filter_map{|v| pos == 0 \
+      ? v
+      : calc(v << 3, pos - 1, program)}
+    .first
 end
 
-puts calc(0, 15, [2,4,1,2,7,5,4,5,1,3,5,5,0,3,3,0])
+program = [2,4,1,2,7,5,4,5,1,3,5,5,0,3,3,0]
+puts calc(0, program.size - 1, program)
