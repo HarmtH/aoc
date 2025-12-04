1a3b703472e582bc4c6b32332d6bee2c48d8f2c5
Harm te Hennepe <dhtehennepe@gmail.com>
2025-12-05 00:59:00 +0100

    04/2025

---
+++ 2025/04a.cpp
@@ -0,0 +1,20 @@
+#include "lib/prelude.hpp"
+#include "lib/point.hpp"
+
+int main(void) {
+    int ans{0};
+    vector<string> grid;
+    string line;
+    while (getline(cin, line)) {
+        grid.eb(line);
+    }
+    for (auto y{0}; y < grid.size(); y++) {
+        for (auto x{0}; x < grid[y].size(); x++) {
+            if (grid[y][x] != '@') continue;
+            auto forklift_nbs = point_t{y, x}.neighbours_on(grid) | v::filter([&grid](auto& p){ return p.value_on(grid) == '@'; });
+            ans += r::distance(forklift_nbs) < 4;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
