120f706506735dab61445594e51cb59ec23dd1f9
Harm te Hennepe <dhtehennepe@gmail.com>
2025-12-05 21:04:52 +0100

    Add some shortcuts

--- 2025/lib/prelude.hpp
+++ 2025/lib/prelude.hpp
@@ -3,6 +3,10 @@
 #define eb emplace_back
 #define pb push_back
 
+#define pll pair<long, long>
+#define fi first
+#define se second
+
 using namespace std;
 namespace r = std::ranges;
 namespace v = std::views;
