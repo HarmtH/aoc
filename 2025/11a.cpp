e1dee31311d364247ed53b3f4c41e3e58614faed
Harm te Hennepe <dhtehennepe@gmail.com>
2025-12-12 19:24:16 +0100

    Shorter is not better (11/2025)

--- 2025/11a.cpp
+++ 2025/11a.cpp
@@ -1,15 +1,13 @@
 #include "lib/prelude.hpp"
 
-int solve(const string& in, const auto& connections) {
+map<string, vector<string>> connections;
+
+int solve(const string& in) {
     if (in == "out") return 1;
-    int sub_ans{0};
-    for (const auto& out : connections.at(in))
-        sub_ans += solve(out, connections);
-    return sub_ans;
+    return r::fold_left(connections.at(in) | v::transform(solve), 0, plus{});
 }
 
 int main(void) {
-    map<string, vector<string>> connections;
     for (string line; getline(cin, line);) {
         stringstream ss{line};
         string in; ss >> in; in.pop_back();
@@ -17,6 +15,6 @@ int main(void) {
         while (ss >> out) { outs.eb(out); }
         connections[in] = std::move(outs);
     }
-    cout << solve("you", connections) << endl;
+    cout << solve("you") << endl;
     return 0;
 }
--- 2025/11b.cpp
+++ 2025/11b.cpp
@@ -1,23 +1,17 @@
 #include "lib/prelude.hpp"
 
-long solve(const string& in, const auto& connections, char visits = {}, map<pair<string, char>, long>&& cache = {}) {
-    if (auto cached_ans = cache.find({in, visits}); cached_ans != cache.end())
-        return cached_ans->se;
-    if (in == "out")
-        return (visits == 0b11) ? 1 : 0;
-    if (in == "dac")
-        visits |= 0b01;
-    if (in == "fft")
-        visits |= 0b10;
-    long sub_ans{0};
-    for (const auto& out : connections.at(in))
-        sub_ans += solve(out, connections, visits, std::forward<decltype(cache)>(cache));
-    cache[{in, visits}] = sub_ans;
-    return sub_ans;
+map<string, vector<string>> connections;
+map<pair<string, char>, long> cache;
+
+long solve(const string& in, char visits = {}) {
+    if (cache.contains({in, visits})) return cache[{in, visits}];
+    if (in == "out") return (visits == 0b11);
+    if (in == "dac") visits |= 0b01;
+    if (in == "fft") visits |= 0b10;
+    return cache[{in, visits}] = r::fold_left(connections.at(in) | v::transform(bind(solve, _1, visits)), 0, plus{});
 }
 
 int main(void) {
-    map<string, vector<string>> connections;
     for (string line; getline(cin, line);) {
         stringstream ss{line};
         string in; ss >> in; in.pop_back();
@@ -25,6 +19,6 @@ int main(void) {
         while (ss >> out) outs.eb(out);
         connections[in] = std::move(outs);
     }
-    cout << solve("svr", connections) << endl;
+    cout << solve("svr") << endl;
     return 0;
 }
